34837fe059a18b1e31c7d1bc38696b4e681177b7
Micah <micahjank@gmail.com>
2020-07-15 16:31:50 -0500

    stuff

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,8 +1,10 @@
 import random
 
+words_list = None
 # Read in all the words in one go
 with open("input.txt") as f:
     words = f.read()
+    words_list = words.split(" ")
 
 # TODO: analyze which words can follow other words
 # Your code here
