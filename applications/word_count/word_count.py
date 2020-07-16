7107cf55e6a0c8162ec53e6bef5af66ed71cab9b
MicahJank <MicahJank@users.noreply.github.com>
2020-07-15 19:01:12 -0500

    got word count workinggit add .git add .git add .!

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -2,33 +2,28 @@
 ignore = ['"', ':', ';', ',', '.', '-', '+', '=', '/', '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
 
 def remove_bad_char(content):
-    for char in content:
-        # print(char)
-        if char in ignore:
-            content = content.replace(char, "")
+    for char in ignore:
+        content = content.replace(char, "")
            
     return content
 
-    # take the string and split it using the split method - this will give me a list of the words
-    # loop over the list of words and iterate over the individual strings to remove any unwanted characters
-    # lowercase the word
-    # check if the word is already in the cache of words - if its increase the count by 1 - if it is not add it to the cache and initialize it to 1
-    # after that return the cache of words
+    # take the string and make everything lowercase
+    # remove all the bad characters from the string
+    # split the string into a list of words i can iterate over
+    # check to see if any of the words are empty - if they are i can skip them
+    # check and see if the word is in the dictionary - if it is then add one to its counter
+    # if it is not then add it to the dictionary with 1 as its value
 def word_count(s):
     word_dict = {}
-    # if check_ignored_char(s) == False:
-    #     return {}
-    s = remove_bad_char(s)
-    word_list = s.split(" ")
-    # print("word list", word_list)
+
+    update_s = s.lower()
+    update_s = remove_bad_char(update_s)
+    word_list = update_s.split()
+
     for word in word_list:
-        # word = remove_bad_char(word)
         if word == "":
             continue
 
-        word = word.lower()
-        
-        
         if word in word_dict:
             word_dict[word] += 1
         else:
@@ -41,9 +36,9 @@ def word_count(s):
 
 
 if __name__ == "__main__":
-    # print(word_count(""))
-    # print(word_count("Hello"))
-    # print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    # print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(""))
+    print(word_count("Hello"))
+    print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
+    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
     # print(word_count("hello \ my friend \hello \again"))
     print(word_count('a a\ra\na\ta \t\r\n'))
